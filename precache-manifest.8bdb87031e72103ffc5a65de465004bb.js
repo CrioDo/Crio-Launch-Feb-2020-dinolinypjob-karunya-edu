1d4a7a92b0b69b8b75cec9b1aeea6bd39b4041dc
Somnath <somnath@Somnaths-MacBook-Air.local>
2020-04-13 18:04:26 +0530

    Updates

---
+++ precache-manifest.8bdb87031e72103ffc5a65de465004bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea83018462c8a4a57891a867051b5f9d",
+    "url": "/Crio-Launch-Feb-2020-dinolinypjob-karunya-edu/index.html"
+  },
+  {
+    "revision": "f21a6e668a6e4e3f9094",
+    "url": "/Crio-Launch-Feb-2020-dinolinypjob-karunya-edu/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "2161aafed03a4663d989",
+    "url": "/Crio-Launch-Feb-2020-dinolinypjob-karunya-edu/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "f21a6e668a6e4e3f9094",
+    "url": "/Crio-Launch-Feb-2020-dinolinypjob-karunya-edu/static/js/2.b42f6e64.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-Launch-Feb-2020-dinolinypjob-karunya-edu/static/js/2.b42f6e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2161aafed03a4663d989",
+    "url": "/Crio-Launch-Feb-2020-dinolinypjob-karunya-edu/static/js/main.2ae85ed6.chunk.js"
+  },
+  {
+    "revision": "4f68d8ae2d25677ebbaa",
+    "url": "/Crio-Launch-Feb-2020-dinolinypjob-karunya-edu/static/js/runtime-main.fefdd9d6.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-Launch-Feb-2020-dinolinypjob-karunya-edu/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-Launch-Feb-2020-dinolinypjob-karunya-edu/static/media/crio-logo.63be71d0.svg"
+  }
+]);
